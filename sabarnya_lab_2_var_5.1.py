3eb34cc5266a5760c2081347482e62021fec1745
Anna25042007 <ann.sabarnya@gmail.com>
2026-03-18 15:26:54 +0200

    done

--- sabarnya_lab_2_var_5.1.py
+++ sabarnya_lab_2_var_5.1.py
@@ -1,1 +1,22 @@
-print("hello")
+class Transport:
+    pass
+class Land(Transport):
+    def get_info(self):
+        return "це наземний транспорт"
+class Water(Transport):
+    def get_info(self):
+        return "це водний транспорт"
+class Auto(Land):
+    pass
+class Boat(Water):
+    pass
+class Gibrid(Land, Water):
+    pass
+
+# Перевірка результатів
+boat = Boat()
+gibrid = Gibrid()
+auto = Land()
+print(f"Boat: {boat.get_info()}")
+print(f"Gibrid: {gibrid.get_info()}")
+print(f"Auto; {auto.get_info()}")
